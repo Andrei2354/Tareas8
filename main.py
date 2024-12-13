f81db157e41657926671e4cb88fea493561bf5a6
Andrei2354 <andreiaimar1362@gmail.com>
2024-12-13 13:58:23 +0000

    sdrfsdf

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from crypt import methods
 
 import psycopg2
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from werkzeug.serving import connection_dropped_errors
 
 app = Flask(__name__)
 
@@ -13,45 +14,65 @@ def ejecutar_sql(sql_text):
     user = "postgres"
     password = "postgres"
 
-    try:
-        connection = psycopg2.connect(
-            host=host,
-            port=port,
-            dbname=dbname,
-            user=user,
-            password=password,
-            options="-c search_path=public"
-        )
-        # Crear un cursor para ejecutar
-        cursor = connection.cursor()
+    connection = psycopg2.connect(
+        host=host,
+        port=port,
+        dbname=dbname,
+        user=user,
+        password=password,
+    )
+    cursor = connection.cursor()
 
-        # Consulta SQL (por ejemplo, selecciona todos los registros de una tabla llamada usuarios)
-        cursor.execute(sql_text)
+    cursor.execute(sql_text)
 
-        # Obtener columnas para contruir claves del JSON
-        columnas = [desc[0] for desc in cursor.description]
-
-        # Convertir resultados a JSON
-        resultados = cursor.fetchall()
-        empleados = [dict(zip(columnas, fila)) for fila in resultados]
-
-        # Cerrar el cursor y la conexión
+    if 'INSERT' in sql_text:
+        connection.commit()
         cursor.close()
         connection.close()
+        return jsonify({'msg': 'insertado'})
 
-        return jsonify(empleados)
+    columnas = [desc[0] for desc in cursor.description]
 
-    except psycopg2.Error as e:
-        print("Error", e)
+    resultados = cursor.fetchall()
+    empleados = [dict(zip(columnas, fila)) for fila in resultados]
 
+    cursor.close()
+    connection.close()
+    return jsonify(empleados)
 
+@app.route('/Prueba',methods=['GET'])
+def otraCosa():
+    resultado = ejecutar_sql(
+    'SELECT * FROM public."Empleado" ORDER BY id ASC LIMIT 100')
 
-@app.route('/empleados', methods=['GET'])
-def obtener_lista_empleados():
-    return ejecutar_sql(
-        'SELECT * FROM public."Empleado" ORDER BY id ASC LIMIT 100'
-    )
+    return resultado
 
+@app.route('/empleado/empleados',methods=['GET'])
+def puestos():
+    resultado = ejecutar_sql(
+    '''SELECT e.nombre,g.id AS id_puesto, 'Gestor' AS rol FROM "Empleado" e INNER JOIN "Gestor" g ON e.id = g.empleado UNION ALL SELECT e.nombre, p.id AS id_puesto,'Programador' AS rol FROM  "Empleado" e INNER JOIN "Programador" p ON e.id = p.empleado;''')
+
+    return resultado
+
+@app.route('/ejemplo_post', methods=['POST'])
+def gestor_ejemplo_post():
+    body_request = request.json
+    nombre = body_request["nombre"]
+    descripcion = body_request["descripcion"]
+    fecha_inicio = body_request["fecha_inicio"]
+    cliente = body_request["cliente"]
+    sql = f"""
+        INSERT INTO public."Proyecto" (nombre, descripcion, fecha_creacion, fecha_inicio, fecha_finalizacion, cliente)
+        VALUES (
+            '{nombre}',
+            '{descripcion}',
+            NOW(),
+            '{fecha_inicio}',
+            null,
+            {cliente},
+        );
+    """
+    return ejecutar_sql(sql)
 
 
 if __name__=='__main__':
