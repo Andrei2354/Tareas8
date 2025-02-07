b7aee7ba7a4a24a434b5b4fba879955337d33bee
Andrei2354 <andreiaimar1362@gmail.com>
2025-02-07 13:22:57 +0000

    comiteo para cambiar usuario a user

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from syslog import LOG_INFO
-
 import psycopg2
 from flask import Flask, jsonify, request
 
@@ -148,6 +146,12 @@ def login_user():
             "email": empleado.json[0]["email"]
         }
     )
+"""
+{
+    "user": "gest1",
+    "passwd":"d404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db"
+}
+"""
 
 
 # Crear tareas a proyectos (debe estar asignado)
